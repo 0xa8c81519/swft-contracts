c636d02da5525c66979af5bd8c53159b66a6a6b7
0xa8c81519 <0xa8c81519@gmail.com>
2021-04-16 13:14:56 +0800

    make some improve after test on mainnet of BSC

--- agg-proxy-demo/index.js
+++ agg-proxy-demo/index.js
@@ -6,6 +6,7 @@ const BigNumber = require('bignumber.js');
 const log4js = require('log4js');
 const config = require('./conf/conf.js');
 const apiModule = require('./api.js');
+const { bn } = require("date-fns/locale");
 
 log4js.configure(config.log4jsConfig);
 const logger = log4js.getLogger('Aggregators Proxy Demo');
@@ -25,54 +26,78 @@ const wallet = walletMnemonic.connect(provider);
 const aggregatorsProxyContract = new ethers.Contract(aggProxyAddress, AggregatorsProxyJson.abi, wallet);
 
 let done = false;
-let filter = aggregatorsProxyContract.filters.Swap(null, null, walletMnemonic.address, null, null, null, null);
+let filter = aggregatorsProxyContract.filters.Swap(null, null, null, null, null, null, null);
 // 监听一下swap调用成功后返回的数据
 aggregatorsProxyContract.on(filter, (fromToken, toToken, sender, fromAmount, minReturnAmount, returnAmount, target) => {
     logger.info('swap record:\nfromToken: ' + fromToken + '\ntoToken: ' + toToken + '\nsender: ' + sender + '\n fromAmount: ' + fromAmount + '\nminReturnAmount: ' + minReturnAmount + '\nreturnAmount: ' + returnAmount + '\ntarget: ' + target);
-    down = true;
+    done = true;
 });
 
 
 let api = apiModule.init(log4js, config.default);
 
+let bnb = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 let cake = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
 let busd = '0x55d398326f99059ff775485246999027b3197955';
 let oneInchRouter = '0x11111112542d85b3ef69ae05771c2dccff4faa26';
 let zeroExRouter = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF';
-let slippage = new BigNumber(0.01);
+let slippage = 0.01;
 
 let denominator = new BigNumber(10).exponentiatedBy(18);
 let amountWei = new BigNumber(10).multipliedBy(denominator);
 
 let pArr = new Array();
 // 调用1inch和0x的api
-pArr.push(api.oneInch.swap(cake, busd, amountWei.toFixed(0), aggProxyAddress));
-pArr.push(api.zeroEx.quote(cake, busd, amountWei.toFixed(0)));
+// 1 cake 换 busd
+// pArr.push(api.oneInch.swap(cake, busd, amountWei.toFixed(0), aggProxyAddress));
+// pArr.push(api.zeroEx.quote(cake, busd, amountWei.toFixed(0)));
+// 0.05 bnb swap busd
 
-Promise.all(pArr).then(res => {
-    // 两个api都返回结果之后
-    let oneInchPrice = new BigNumber(res[0].price);
-    let zeroExPrice = new BigNumber(res[1].toTokenAmount).div(denominator);
-    // 比较价格，选最优的api返回的data数据
-    let data = oneInchPrice.comparedTo(zeroExPrice) > 0 ? res[0].data : res[1].tx.data;
-    let minReturn = oneInchPrice.comparedTo(zeroExPrice) > 0 ? oneInchPrice : zeroExPrice;
-    let approveTarget = oneInchPrice.comparedTo(zeroExPrice) > 0 ? oneInchRouter : zeroExRouter;
-    let deaLine = Math.floor(new Date() / 1000) + 20 * 60;
-    // 跟前端是一样的。先估算gas
-    aggregatorsProxyContract.estimate.swap(cake, busd, approveTarget, amountWei.toFixed(0), minReturn.multipliedBy(denominator).toFixed(0), data, deaLine, { from: walletMnemonic.address }).then(gas => {
-        // 调用链上合约
-        aggregatorsProxyContract.connect(wallet).swap(cake, busd, approveTarget, amountWei.toFixed(0), minReturn.multipliedBy(denominator).toFixed(0), data, deaLine).thne(res => {
-            logger.info('Swap tx is send.');
-        }).catch(e => {
-            logger.error(e);
+(function swapBnbToBusd() {
+    let amtWei = new BigNumber(0.05).multipliedBy(denominator).toFixed(0);
+    pArr.push(api.oneInch.swap(bnb, busd, amtWei, aggProxyAddress));
+    pArr.push(api.zeroEx.quote(bnb, busd, amtWei));
+
+    Promise.all(pArr).then(res => {
+        // 两个api都返回结果之后
+        let oneInchPrice = new BigNumber(res[0].toTokenAmount).div(res[0].fromTokenAmount);
+        let zeroExPrice = new BigNumber(res[1].price);
+        logger.info('1inch price: ' + oneInchPrice.toFormat(9, BigNumber.ROUND_DOWN));
+        logger.info('zeroEx price: ' + zeroExPrice.toFormat(9, BigNumber.ROUND_DOWN));
+        // 比较价格，选最优的api返回的data数据
+        let data = oneInchPrice.comparedTo(zeroExPrice) > 0 ? res[0].tx.data : res[1].data;
+        let minReturn = oneInchPrice.comparedTo(zeroExPrice) > 0 ? oneInchPrice.multipliedBy(amtWei).multipliedBy(1 - slippage) : zeroExPrice.multipliedBy(amtWei).multipliedBy(1 - slippage);
+        logger.debug('min return amt: ' + minReturn.toFormat(0, 1));
+        let approveTarget = oneInchPrice.comparedTo(zeroExPrice) > 0 ? oneInchRouter : zeroExRouter;
+        let deadLine = Math.floor(new Date() / 1000) + 20 * 60;
+        aggregatorsProxyContract.isWhiteListed(approveTarget).then(async isWhiteListed => {
+            if (!isWhiteListed) {
+                logger.error("Not white listed.");
+                logger.error("Approve target: " + approveTarget);
+                // await aggregatorsProxyContract.addWhiteList(zeroExRouter);
+                // await aggregatorsProxyContract.addWhiteList(oneInchRouter);
+            } else {
+                let valHex = new ethers.utils.BigNumber(amtWei).toHexString();
+                logger.debug('value hex: ' + valHex);
+                // 跟前端是一样的。先估算gas
+                aggregatorsProxyContract.estimate.swap(bnb, busd, approveTarget, amountWei.toFixed(0), minReturn.toFixed(0), data, deadLine, { value: valHex }).then(gas => {
+                    // 调用链上合约
+                    aggregatorsProxyContract.connect(wallet).swap(bnb, busd, approveTarget, amountWei.toFixed(0), minReturn.toFixed(0), data, deadLine, { value: valHex, gasLimit: gas }).then(res => {
+                        logger.info('Swap tx is send.');
+                    }).catch(e => {
+                        logger.error(e);
+                    });
+                }).catch(e => {
+                    logger.error(e);
+                });
+            }
         });
-    }).catch(e => {
-        logger.error(e);
     });
-});
+})();
 
-for (; true === true;) { // 程序做成守护进程，直到调用swap确认后推出
-    if (done) {
-        break;
-    }
-}
+
+// for (; true === true;) { // 程序做成守护进程，直到调用swap确认后推出
+//     if (done) {
+//         break;
+//     }
+// }
---
+++ agg-proxy-demo/set-opt.js
@@ -0,0 +1,42 @@
+"use strict"
+const ethers = require("ethers");
+const AggregatorsProxyJson = require('../build/contracts/AggregatorsProxy.json');
+const { mnemonic } = require('../secret.js');
+const BigNumber = require('bignumber.js');
+const log4js = require('log4js');
+const config = require('./conf/conf.js');
+const { bn } = require("date-fns/locale");
+
+log4js.configure(config.log4jsConfig);
+const logger = log4js.getLogger('Seting Contract');
+logger.info('Aggregators Proxy Demo started.');
+
+// 合约地址
+const aggProxyAddress = config.default.aggregatorsProxy.address;
+
+// 初始化rpc provider，浏览器中不需要
+const provider = new ethers.providers.JsonRpcProvider(config.default.rpc.url);
+// 初始化助记词
+const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic);
+// 初始化钱包
+const wallet = walletMnemonic.connect(provider);
+
+// 初始化聚合器代理合约，浏览器中方法是一样的。
+const aggregatorsProxyContract = new ethers.Contract(aggProxyAddress, AggregatorsProxyJson.abi, wallet);
+
+let oneInchRouter = '0x11111112542d85b3ef69ae05771c2dccff4faa26';
+let zeroExRouter = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF';
+
+(async function doSetting() {
+    // await aggregatorsProxyContract.addWhiteList(zeroExRouter);
+    // await aggregatorsProxyContract.addWhiteList(oneInchRouter);
+    // await aggregatorsProxyContract.setDev(walletMnemonic.address);
+    await aggregatorsProxyContract.setFee('3000000000000000');
+})();
+
+
+// for (; true === true;) { // 程序做成守护进程，直到调用swap确认后推出
+//     if (done) {
+//         break;
+//     }
+// }
